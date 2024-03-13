8b71a7ab10872bbeccbcaca1b3d1c4bbddaae5e1
Magnus Christian Larsen <happymansi@hotmail.com>
2024-03-13 12:35:44 +0100

    Changed pnpm

--- run_and_deploy.sh
+++ run_and_deploy.sh
@@ -3,22 +3,23 @@ chmod +x ./run_and_deploy.sh
 apt-get update
 apt-get install bash -y
 apt-get install git -y
-yarn install
+npm install -g pnpm
+pnpm install
 
 echo "Prisma Generate";
-yarn db:deploy;
+pnpm db:deploy;
 
 echo "The value of DEV_ENV is: $DEV_ENV"
 
 if [ "$DEV_ENV" = "YES" ]
 then
     echo "Starting Server in dev env"
-    yarn dev
+    pnpm dev
 else 
     echo "Building backend";
-    yarn build;
+    pnpm build;
     echo Starting Server
-    yarn start
+    pnpm start
 fi
 
 
