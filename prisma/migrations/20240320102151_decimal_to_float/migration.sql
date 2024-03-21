ec70cbbffaba9c58349995e27eb70eeac9e611ca
Magnus Christian Larsen <happymansi@hotmail.com>
2024-03-21 11:34:55 +0100

    Updated schema and migrations

--- prisma/migrations/20240320102151_decimal_to_float/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `wattage` on the `SmartPlugMeasurement` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `DoublePrecision`.
-
-*/
--- AlterTable
-ALTER TABLE "SmartPlugMeasurement" ALTER COLUMN "wattage" SET DATA TYPE DOUBLE PRECISION;
